051b67d1ffa4bff1f30eec924ac4beea78fbca06
Beach Dix <beach.dix.work@gmail.com>
2020-10-22 18:37:30 -0400

    try a different example

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,39 +1,30 @@
-  
 import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
 
-export function run(): Promise<void> {
+export function run(testsRoot: string, cb: (error: any, failures?: number) => void): void {
 	// Create the mocha test
 	const mocha = new Mocha({
 		ui: 'tdd'
 	});
-	// mocha.useColors(true);
+	mocha.useColors(true);
 
-	const testsRoot = path.resolve(__dirname, '..');
+	glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+		if (err) {
+			return cb(err);
+		}
 
-	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
+		// Add files to the test suite
+		files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				e(err);
-			}
-		});
+		try {
+			// Run the mocha test
+			mocha.run(failures => {
+				cb(null, failures);
+			});
+		} catch (err) {
+			console.error(err);
+			cb(err);
+		}
 	});
 }
